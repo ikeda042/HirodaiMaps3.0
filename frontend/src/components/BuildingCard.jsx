867dbc3f45e10ff7fc8abef914b47966e931e511
ikeda042 <ikeda042main@gmail.com>
2023-12-01 22:44:21 +0900

    Image Margin

--- frontend/src/components/BuildingCard.jsx
+++ frontend/src/components/BuildingCard.jsx
@@ -9,22 +9,31 @@ import Typography from '@mui/material/Typography';
 function BuildingCard({ title, description, imageUrl, buildingId}) {// 例: 'building1.png' => '1'
 
   return (
-    <Link to={`/building/${buildingId}`}>
+    <Link to={`/building/${buildingId}`} style={{ textDecoration: 'none',color: 'inherit', fontFamily: 'Arial'}}>
        <div style={{ margin: '0 1%' }}> {/* 左右のマージンを1%に設定 */}
       <Card sx={{ width: '100%', display: 'flex',  margin: '0.1% auto'  }}>
         <CardMedia
-          component="img"
-          sx={{ width: 100, height: 100 }}  
-          image={imageUrl}
-          alt="Building Image"
+           component="img"
+           sx={{ width: 100, height: 100, padding: '0.4%' }}
+           image={imageUrl}
+           alt="Building Image"
         />
         <CardContent sx={{ flex: 1 }}>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            height: '100%' // Ensure the container takes the full height
+          }}
+        >
+          {title}
+        </Typography>
+          {/* <Typography variant="body2" color="text.secondary">
             {description}
-          </Typography>
+          </Typography> */}
         </CardContent>
       </Card>
     </div>
