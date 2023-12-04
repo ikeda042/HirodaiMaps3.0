e877cf3e0d5670b7860a2157e19bb058389fd88b
ikeda042 <ikeda042main@gmail.com>
2023-12-05 02:19:16 +0900

    MenuBar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,9 +6,20 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 
 export default function ButtonAppBar(props) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: '#005e3c' }}>
@@ -19,14 +30,40 @@ export default function ButtonAppBar(props) {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleMenu}
           >
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                backgroundColor: 'rgba(255, 255, 255, 0.9)', 
+              },
+            }}
+          >
+            {/* メニューアイテムをここに追加 */}
+            <MenuItem onClick={handleClose}>学部</MenuItem>
+            <MenuItem onClick={handleClose}>研究室</MenuItem>
+            <MenuItem onClick={handleClose}>お知らせ</MenuItem>
+
+          </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/" style={{ textDecoration: 'none',color: 'inherit', fontFamily: 'Arial'}}>
-          Hirodai Maps 3.0 {props.headerText}
-          </Link>
-           
+            <Link to="/" style={{ textDecoration: 'none',color: 'inherit', fontFamily: 'Arial'}}>
+            Hirodai Maps 3.0 {props.headerText}
+            </Link>
           </Typography>
           <Button color="inherit" href='https://www.hiroshima-u.ac.jp/'>広島大学</Button>
         </Toolbar>
