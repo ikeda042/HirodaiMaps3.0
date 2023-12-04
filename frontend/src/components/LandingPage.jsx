b2efa30fa11c437f4375632ccf66876df0514d05
ikeda042 <ikeda042main@gmail.com>
2023-12-05 01:20:50 +0900

    LP

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import ButtonAppBar from "./Navbar";
 import SearchBar from "./Searchbar";
 import BuildingCard from "./BuildingCard";
+import BottomNav from "./BottomNavigation ";
 import React, { useState, useEffect } from 'react';
 
 function LandingPage(props) {
@@ -12,7 +13,7 @@ function LandingPage(props) {
       title: '総合科学部棟',
       description: '総合科学部の講義室がある棟です。',
       imageUrl: 'building_images/building1.png',
-      keywords: ['総合科学部', '総合科学部棟', '総合科学部講義室','そうかとう'],
+      keywords: ['総合科学部', '総合科学部棟', '総合科学部講義室','そうかとう',"L102"],
     },
     {
       buildingId: '2',
@@ -42,7 +43,6 @@ function LandingPage(props) {
       imageUrl: 'building_images/building5.png',
       keywords: ['文学部', '文学部棟', '文学部講義室', 'ぶんがくぶ'],
     },
-   
   ];
 
   useEffect(() => {
@@ -70,38 +70,7 @@ function LandingPage(props) {
           imageUrl={building.imageUrl}
         />
       ))}
-      {/* <ButtonAppBar headerText={props.headerText}/>
-      <SearchBar />
-      <BuildingCard
-        buildingId={'1'}
-        title="総合科学部棟"
-        description="総合科学部の講義室がある棟です。"
-        imageUrl="building_images/building1.png"
-      />
-       <BuildingCard
-        buildingId={'2'}
-        title="工学部第3類A4棟"
-        description="工学部第３類の講義室、研究室がある棟です。"
-        imageUrl="building_images/building2.png"
-      />
-       <BuildingCard
-        buildingId={'3'}
-        title="先端物質科学研究科"
-        description="統合生命科学研究科の講義室、研究室がある棟です。"
-        imageUrl="building_images/building3.png"
-      />
-       <BuildingCard
-        buildingId={'4'}
-        title="教育学部K棟"
-        description="教育学部の講義室がある棟です。"
-        imageUrl="building_images/building4.png"
-      />
-       <BuildingCard
-        buildingId={'5'}
-        title="文学部棟"
-        description="文学部の講義室がある棟です。"
-        imageUrl="building_images/building5.png"
-      /> */}
+      <BottomNav />
     </div>
   );
 }
