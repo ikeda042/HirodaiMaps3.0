e91336bdb12ad525303ce384f7d49d6d89480b4d
ikeda042 <ikeda042main@gmail.com>
2023-12-04 23:22:43 +0900

    Search

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,12 +1,69 @@
 import ButtonAppBar from "./Navbar";
 import SearchBar from "./Searchbar";
 import BuildingCard from "./BuildingCard";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function LandingPage(props) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredBuildings, setFilteredBuildings] = useState([]);
+  const buildings = [
+    {
+      buildingId: '1',
+      title: '総合科学部棟',
+      description: '総合科学部の講義室がある棟です。',
+      imageUrl: 'building_images/building1.png',
+    },
+    {
+      buildingId: '2',
+      title: '工学部第3類A4棟',
+      description: '工学部第３類の講義室、研究室がある棟です。',
+      imageUrl: 'building_images/building2.png',
+    },
+    {
+      buildingId: '3',
+      title: '先端物質科学研究科',
+      description: '統合生命科学研究科の講義室、研究室がある棟です。',
+      imageUrl: 'building_images/building3.png',
+    },
+    {
+      buildingId: '4',
+      title: '教育学部K棟',
+      description: '教育学部の講義室がある棟です。',
+      imageUrl: 'building_images/building4.png',
+    },
+    {
+      buildingId: '5',
+      title: '文学部棟',
+      description: '文学部の講義室がある棟です。',
+      imageUrl: 'building_images/building5.png',
+    },
+  ];
+
+  useEffect(() => {
+    // ここで buildings は全ての建物のデータを含む配列です
+    const filtered = buildings.filter(building =>
+      building.title.includes(searchQuery)
+    );
+    setFilteredBuildings(filtered);
+  }, [searchQuery]);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
   return (
     <div className="App">
-      <ButtonAppBar headerText={props.headerText}/>
+        <ButtonAppBar headerText={props.headerText}/>
+      <SearchBar onSearch={handleSearch} />
+      {filteredBuildings.map(building => (
+        <BuildingCard
+          key={building.buildingId}
+          buildingId={building.buildingId}
+          title={building.title}
+          description={building.description}
+          imageUrl={building.imageUrl}
+        />
+      ))}
+      {/* <ButtonAppBar headerText={props.headerText}/>
       <SearchBar />
       <BuildingCard
         buildingId={'1'}
@@ -37,7 +94,7 @@ function LandingPage(props) {
         title="文学部棟"
         description="文学部の講義室がある棟です。"
         imageUrl="building_images/building5.png"
-      />
+      /> */}
     </div>
   );
 }
--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    onSearch(event.target.value);
+  };
   return (
-    <div style={{ margin: '10px' }}> {/* 上下左右に10ピクセルのマージンを追加 */}
+    <div style={{ margin: '10px' }}> 
       <TextField
         fullWidth
         placeholder="学部、建物、講義室で検索(e.g., 総合科学部）"
         variant="outlined"
+        value={searchQuery}
+        onChange={handleSearchChange}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
