596e0ece6d9fec9fa07a871e997b058bd4e53027
ikeda042 <ikeda042main@gmail.com>
2023-12-06 13:45:19 +0900

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,15 +54,14 @@ export default function ButtonAppBar(props) {
               },
             }}
           >
-            {/* メニューアイテムをここに追加 */}
             <MenuItem onClick={handleClose}>学部</MenuItem>
             <MenuItem onClick={handleClose}>研究室</MenuItem>
             <MenuItem onClick={handleClose}>お知らせ</MenuItem>
 
           </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/" style={{ textDecoration: 'none',color: 'inherit', fontFamily: 'Arial'}}>
-            Hirodai Maps 3.0 {props.headerText}
+            <Link to={`/?qrid=${props.checkpointID}`} style={{ textDecoration: 'none',color: 'inherit', fontFamily: 'Arial'}}>
+            HirodaiMaps{props.headerText}
             </Link>
           </Typography>
           <Button color="inherit">Checkpoint:{props.checkpointID}</Button>
