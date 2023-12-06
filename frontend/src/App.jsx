ce7427231ae3c04f29ccc83749b1e1a69965c5e0
ikeda042 <ikeda042main@gmail.com>
2023-12-06 13:40:30 +0900

    バリアフリーメニュー

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from "./components/LandingPage";
 import BuildingDetail from "./components/BuildingDetail";
 import buildings from './components/buildingsData';
-
+import AccessibilityMenu from './components/AccessibilityMenu';
 
 const itemData = buildings;
 
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/building/:buildingId" element={<BuildingDetail detailData={itemData} />} />
+          <Route path="/accessibilitymenu" element={<AccessibilityMenu />} />
         </Routes>
       </div>
     </Router>
