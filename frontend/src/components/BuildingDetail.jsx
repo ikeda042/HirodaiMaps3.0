0ffb3a96cbbe6b96326aad9815ebb472bf1c3e66
ikeda042 <ikeda042main@gmail.com>
2023-12-13 03:11:22 +0900

    BD

--- frontend/src/components/BuildingDetail.jsx
+++ frontend/src/components/BuildingDetail.jsx
@@ -55,7 +55,7 @@ function BuildingDetail({ detailData }) {
       <iframe
         id="mapIframe"
         src={"/map/map.html"+ `?lat=${lat_query}&lon=${lon_query}&title=${title}`}
-        style={{ height: '350px', width: '350px', border: '2px solid black', marginTop: '20px', marginBottom: '90px' }} // 枠とマージンを追加
+        style={{ height: '350px', width: '350px', border: '2px solid black', marginTop: '20px', marginBottom: '90px' }} 
         title="Map"
       ></iframe>
     </div>
