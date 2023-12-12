dcd48435daf77b2b8e11fc74893a80f50d33fdf1
ikeda042 <ikeda042main@gmail.com>
2023-12-13 02:29:56 +0900

    LP

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -5,6 +5,7 @@ import BottomNav from "./BottomNavigation";
 import React, { useState, useEffect } from 'react';
 import buildings from './buildingsData';
 import { useNavigate } from 'react-router-dom';
+import checkpoints from "./CheckpointData";
 
 function LandingPage(props) {
   const [searchQuery, setSearchQuery] = useState('');
@@ -19,7 +20,11 @@ function LandingPage(props) {
   const getQridFromQueryParam = param => new URLSearchParams(window.location.search).get(param);
   const QRID = getQridFromQueryParam("qrid");
   const checkpointID = QRID ? QRID : "0";
-  
+  const lat_query = checkpoints.find((item) => item.checkpointId === checkpointID).lat;
+  const lon_query = checkpoints.find((item) => item.checkpointId === checkpointID).lon;
+
+  console.log(lat_query);
+  console.log(lon_query);
   useEffect(() => {
     let filtered;
     if (searchQuery) {
@@ -55,7 +60,7 @@ function LandingPage(props) {
           />
         ))}
       <div style={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1000}}> {/* ここでBottomNavを固定 */}
-      <BottomNav QRID={checkpointID}/>
+      <BottomNav QRID={checkpointID} lat = {lat_query} lon = {lon_query} title = {"title"}/>
     </div>
     </div>
   </div>
