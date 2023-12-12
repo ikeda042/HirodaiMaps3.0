2715956a880151395f7bd2091771fbe93cf1b9e2
ikeda042 <ikeda042main@gmail.com>
2023-12-13 02:30:13 +0900

    Route comp.

---
+++ frontend/src/components/Route.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Route() {
+  return (
+    <div>
+      {/* Your Route component's content */}
+      <h1>Route Page</h1>
+    </div>
+  );
+}
+
+export default Route;
