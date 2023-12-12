c7a183e295b237c56d0237321f0d835f6369aae9
ikeda042 <ikeda042main@gmail.com>
2023-12-13 03:11:35 +0900

    Checkpointall

--- frontend/src/components/CheckpointData.jsx
+++ frontend/src/components/CheckpointData.jsx
@@ -1,7 +1,7 @@
 
 const checkpoints = [
     { 'checkpointId': '0', 'title': 'root', 'description': '郵便局前', 'lat': '34.40320', 'lon': '132.71345' },
-    { 'checkpointId': '1', 'title': 'チェックポイント１', 'description': '郵便局前', 'lat': '34.40320', 'lon': '132.71345' },
+    { 'checkpointId': '1', 'title': 'チェックポイント１', 'description': '郵便局前', 'lat': '34.40555', 'lon': '132.71245' },
   ];
   
   export default checkpoints;
